0de9ed65e46ff5c47915d7fb6276819c4093abed
Tekniikkaatutuksi <veijo.kirkinen@tekniikkaatutuksi.fi>
2020-10-19 13:04:29 +0300

    Päivitys

--- src/com/real3dtech/Main.java
+++ src/com/real3dtech/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Hello!");
+        System.out.println("No moro moro!");
     }
 }
